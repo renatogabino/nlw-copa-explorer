3997f841ba945ddf8f9c5dad217b30b48fe13ab8
renatogabino <renatogd2003@gmail.com>
2022-11-04 14:51:00 -0300

    inicial commit

--- main.js
+++ main.js
@@ -22,11 +22,27 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "7:00", "cameroon") +
+      createGame("uruguay", "10:00", "south-korea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
   createCard(
     "28/11",
     "segunda",
-    createGame("brazil", "13:00", "switzerland") +
+    createGame("cameroon", "7:00", "serbia") +
+      createGame("south-korea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
       createGame("portugal", "16:00", "uruguay")
   ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("south-korea", "7:00", "portugal") +
+      createGame("ghana", "10:00", "uruguay") +
+      createGame("serbia", "13:00", "switzerland") +
+      createGame("brazil", "16:00", "cameroon")
+  )
